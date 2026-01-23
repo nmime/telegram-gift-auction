bc1a00ac9fefc41c40d0608ab09a12aea2fa9de3
nmime <66474195+nmime@users.noreply.github.com>
2026-01-23 14:26:43 +0500

    refactor: use node: prefix for built-in module imports

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,5 +1,5 @@
-import * as cluster from "cluster";
-import * as os from "os";
+import cluster from "node:cluster";
+import os from "node:os";
 import { NestFactory } from "@nestjs/core";
 import {
   FastifyAdapter,
